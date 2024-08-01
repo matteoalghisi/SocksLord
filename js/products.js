460a64de65dd1c6fb72d3fbd8af0eae009b7f39d
Federica Piovesan <f.piovesan1@campus.unimib.it>
2024-08-01 13:58:35 +0200

    filtraggio emporium

--- js/products.js
+++ js/products.js
@@ -1,20 +1,23 @@
 const filters = {
     collabs: [],
     colors: [],
+    functions: [],
+    feet: [],
+    materials: [],
 };
 
-
+let filteredProducts = [];
 const products = [
     { id: 1, name: "sock1", price: 20, imagePath: "img/card/calza_verde_desktop.svg", collabId: 1, colorId: 1, functionId: 1, footId: 1, materialId: 1 },
-    { id: 2, name: "sock2", price: 20, imagePath: "img/card/calza_verde_desktop.svg", collabId: 1, colorId: 1, functionId: 1, footId: 1, materialId: 1 },
-    { id: 3, name: "sock3", price: 20, imagePath: "img/card/calza_verde_desktop.svg", collabId: 1, colorId: 1, functionId: 1, footId: 1, materialId: 1 },
-    { id: 4, name: "sock1", price: 20, imagePath: "img/card/calza_verde_desktop.svg", collabId: 1, colorId: 1, functionId: 1, footId: 1, materialId: 1 },
-    { id: 5, name: "sock1", price: 20, imagePath: "img/card/calza_verde_desktop.svg", collabId: 1, colorId: 1, functionId: 1, footId: 1, materialId: 1 },
+    { id: 2, name: "sock2", price: 20, imagePath: "img/card/calza_verde_desktop.svg", collabId: null, colorId: 2, functionId: 1, footId: 2, materialId: 1 },
+    { id: 3, name: "sock3", price: 20, imagePath: "img/card/calza_verde_desktop.svg", collabId: null, colorId: 3, functionId: 2, footId: 1, materialId: 1 },
+    { id: 4, name: "sock1", price: 20, imagePath: "img/card/calza_verde_desktop.svg", collabId: 1, colorId: 1, functionId: 1, footId: 2, materialId: 1 },
+    { id: 5, name: "sock1", price: 20, imagePath: "img/card/calza_verde_desktop.svg", collabId: 1, colorId: 3, functionId: null, footId: null, materialId: 1 },
 ];
 const collabs = [{ id: 1, name: "Lux" }];
 const colors = [{ id: 1, name: "Red" }, { id: 2, name: "RBlueed" }, { id: 3, name: "Green" }];
-const functions = [{ id: 1, name: "Elica" }];
-const feet = [{ id: 1, name: "One" }];
+const functions = [{ id: 1, name: "Elica" }, { id: 2, name: "GPS" }];
+const feet = [{ id: 1, name: "One" }, { id: 2, name: "Two" }];
 const materials = [{ id: 1, name: "Tessuto" }];
 
 let productsContainer;
@@ -52,19 +55,41 @@ $(() => {
     addFilters();   
     
     $(".filter-modal").on("click", (e) => {
-        const filter = $(e.currentTarget);
-        const filterId = filter.attr("data-id");
-        const type = filter.attr("data-type");
+        const filterElement = $(e.currentTarget);
+        const filterId = filterElement.attr("data-id");
+        const type = filterElement.attr("data-type");
 
         if (filters[type]) {
             const index = filters[type].findIndex(id => id == filterId);
-            if (index != -1) filters[type].splice(index, 1);
-            else filters[type].push(filterId);
+            const spanCircleElement = filterElement.find(".circle-filter");
+            const spanIconElement = filterElement.find(".icon-filter");
+
+            if (index != -1) {
+                filters[type].splice(index, 1);
+                filterElement.removeClass("filter-modal-active");
+                if (spanCircleElement.length) spanCircleElement.removeClass("circle-filter-active");
+                else spanIconElement.removeClass("icon-filter-active");
+            } else {
+                filters[type].push(parseInt(filterId));
+                filterElement.addClass("filter-modal-active");
+                if (spanCircleElement.length) spanCircleElement.addClass("circle-filter-active");
+                else spanIconElement.addClass("icon-filter-active");
+            }
         }
     })
 
     $('[data-toggle="filter"]').on("click", (e) => {
-        
+        filteredProducts = products.filter(product => {
+            if (filters.collabs.length && !filters.collabs.includes(product.collabId)) return false;
+            if (filters.colors.length && !filters.colors.includes(product.colorId)) return false;
+            if (filters.functions.length && !filters.functions.includes(product.functionId)) return false;
+            if (filters.feet.length && !filters.feet.includes(product.footId)) return false;
+            if (filters.materials.length && !filters.materials.includes(product.materialId)) return false;
+
+            return true;
+        });
+       
+    console.log(filteredProducts);
     })
 });
 
@@ -86,7 +111,7 @@ const addFilters = () => {
 
     functions.forEach((functions, index) => {
         buttonsFunctions.append(`
-            <button class="filter-modal d-flex justify-content-center align-items-center">
+            <button class="filter-modal d-flex justify-content-center align-items-center" data-id="${functions.id}" data-type="functions">
                 <img src="img/icon/Propeller.svg" alt="" class="icon-filter">
                 <span class="text-style-button">${functions.name}</span>
             </button>
@@ -95,7 +120,7 @@ const addFilters = () => {
 
     feet.forEach((foot, index) => {
         buttonsFeet.append(`
-            <button class="filter-modal d-flex justify-content-center align-items-center">
+            <button class="filter-modal d-flex justify-content-center align-items-center" data-id="${foot.id}" data-type="feet">
                 <img src="img/icon/piede1.svg" alt="" class="icon-filter">
                 <span class="text-style-button">${foot.name}</span>
             </button>
@@ -104,7 +129,7 @@ const addFilters = () => {
 
     materials.forEach((material, index) => {
         buttonsMaterials.append(`
-            <button class="filter-modal d-flex justify-content-center align-items-center">
+            <button class="filter-modal d-flex justify-content-center align-items-center" data-id="${material.id}" data-type="materials">
                 <img src="img/icon/tessuto.svg" class="circle-filter">
                 <span class="text-style-button">${material.name}</span>
             </button>
